532e7ec875bb2e8824b090ce19a6bf848dbe91c3
v.podkopaev <max.astin@yandex.ru>
2025-02-12 21:51:46 +0200

    update version up to 0.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.github.maxastin.scorecounter"
         minSdk = 24
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.0.5"
+        versionCode = 7
+        versionName = "0.0.7"
         multiDexEnabled = true
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
